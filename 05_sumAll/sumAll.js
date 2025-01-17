4b0fb20351f789ebac31c5b6d9c60eb2469b7729
Guillermo Muñoz <guillemunozi2003@gmail.com>
2025-01-17 11:41:36 -0500

    Summary

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    if (num1 > num2) {
+        [num1, num2] = [num2, num1]
+    }
 
+    for(i = num1 ; i < num2.length; i++){
+        sum += i
+    }
+    return sum
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(x) {
+  const formulaToCelsius = (x - 32) * (5 / 9);
+  let result = Math.round(formulaToCelsius * 10) / 10;
+  return result
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(x) {
+  const formulaToFahrenheit =  x * (9 / 5) + 32;
+  let result = Math.round(formulaToFahrenheit * 10) / 10;
+  return result
+  
 };
 
 // Do not edit below this line
