c8509e14c3ce5a8899b9bea4df6af8f878e3892d
Guillermo Muñoz <guillemunozi2003@gmail.com>
2025-01-17 10:45:37 -0500

    Summary

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    if(num < 0){
+        return 'Error'
+    }
+    let result = ''
 
+    for(i = 0; i < num; i++){
+        return result += str
+    }
+    return result
 };
+
+//const repeatString = (string, num) => 
+//    num < 0 ? 'ERROR' : Array(num).fill(string).join('');
+
 
 // Do not edit below this line
 module.exports = repeatString;
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, itemToRemove) {
+    const index = arr.indexOf(itemToRemove);
+    if (index == -1) {
+        arr.splice(index,1)
+    }
+    return arr
 };
+
+// const removeFromArray = function(array, ...args) {
+//   return array.filter(val => !args.includes(val))
 
 // Do not edit below this line
 module.exports = removeFromArray;
